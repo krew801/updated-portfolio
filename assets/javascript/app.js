f5ea1a5f8fc04480972ebf55f4423394bced8b3f
Alex Smith <alsmith@instructure.com>
2019-09-07 22:34:22 -0600

    added images for project links and changed background. Added json package to attempt page animations

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// import anime from 'animejs/lib/anime.es.js';
+// const anime = require('anime');
+
+
+// anime({
+//     targets: 'div',
+//     translateX: 250,
+//     rotate: '1turn',
+//     backgroundColor: '#FFF',
+//     duration: 800
+//   });
